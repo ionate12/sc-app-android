bdc7f39ca734a2de33c392c349161ceb754c998c
Minh Khoi Mai <mmkhoi.uel@gmail.com>
2022-12-22 18:12:01 +0700

    move classes

--- data/src/main/java/au/com/safetychampion/data/local/BaseAppDataStore.kt
+++ data/src/main/java/au/com/safetychampion/data/local/BaseAppDataStore.kt
@@ -2,11 +2,7 @@ package au.com.safetychampion.data.local
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.core.stringSetPreferencesKey
 import au.com.safetychampion.data.util.IDispatchers
 import au.com.safetychampion.util.koinInject
 import kotlinx.coroutines.flow.firstOrNull
@@ -35,28 +31,5 @@ abstract class BaseAppDataStore {
 
     fun <T : Any> getImmediate(storeKey: StoreKey<T>): T? = runBlocking(dispatchers.io) {
         get(storeKey)
-    }
-}
-
-sealed class StoreKey<T : Any>(protected open val key: String) {
-    abstract fun prefKey(): Preferences.Key<T>
-    sealed class AsString(override val key: String) : StoreKey<String>(key) {
-        override fun prefKey(): Preferences.Key<String> = stringPreferencesKey(key)
-
-        object TokenAuthed : AsString("token_authed")
-        object TokenMorphed : AsString("token_morphed")
-    }
-
-    sealed class AsBoolean(override val key: String) : StoreKey<Boolean>(key) {
-        override fun prefKey(): Preferences.Key<Boolean> = booleanPreferencesKey(key)
-        // To add more boolean value
-    }
-    sealed class AsInt(override val key: String) : StoreKey<Int>(key) {
-        override fun prefKey(): Preferences.Key<Int> = intPreferencesKey(key)
-        // To add more Int Value
-    }
-    sealed class AsStringSet(override val key: String) : StoreKey<Set<String>>(key) {
-        override fun prefKey(): Preferences.Key<Set<String>> = stringSetPreferencesKey(key)
-        // To add more String Set Value
     }
 }
