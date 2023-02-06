daeff10e5c237d963ee26848026b75bdbbfa4079
Khoi <khoi.mai@spenmo.com>
2023-02-06 10:50:31 +0700

    clean up

--- visitor/src/main/java/com/safetychampion/visitor/vm/VisitorRoleSelectionViewModel.kt
+++
@@ -1,8 +0,0 @@
-package au.com.safetychampion.scmobile.visitorModule.vm
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class VisitorRoleSelectionViewModel : ViewModel() {
-    val role = MutableLiveData<String?>()
-}
